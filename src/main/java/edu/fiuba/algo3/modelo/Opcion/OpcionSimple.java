c75625b99f759294ebdf4ae89744b8682b206d1e
salvaPerezMendoza <149404093+salvaPerezMendoza@users.noreply.github.com>
2024-06-23 20:01:55 -0300

    Update OpcionSimple.java

--- src/main/java/edu/fiuba/algo3/modelo/Opcion/OpcionSimple.java
+++ src/main/java/edu/fiuba/algo3/modelo/Opcion/OpcionSimple.java
@@ -1,24 +1,29 @@
 package edu.fiuba.algo3.modelo.Opcion;
 
 public class OpcionSimple implements Opcion {
-    private final String valor;
-    private int id;
-
-    public OpcionSimple(String valor, int id) {
-        this.id = id;
+    String valor;
+    public OpcionSimple(String valor) {
         this.valor = valor;
     }
 
     public String getValor() {
         return valor;
     }
-    public int getId() {
-        return id;
-    }
 
     @Override
     public boolean esIgualA(Opcion opcion) {
         OpcionSimple opcionSimple = (OpcionSimple) opcion;
-        return this.id == opcionSimple.getId();
+        return this.valor.equals(opcionSimple.getValor());
     }
+
+    @Override
+    public String getTexto() {
+        return valor;
+    }
+
+    @Override
+    public String getName() {
+        return valor;
+    }
+
 }
